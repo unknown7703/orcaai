b364bb41db93e3a4395a5a5852901d1fbb6d965d
Aatreya <padmajaatreya02@gmail.com>
2025-03-15 17:41:40 +0530

    feat: terminal socket + req freeze

--- backend/routes/router.py
+++ backend/routes/router.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends , BackgroundTasks
+from fastapi import APIRouter, BackgroundTasks, WebSocket
 from controllers.query_controller import process_query
+from controllers.terminal_socket import ws_controller
 from conductor.conductor import main_conductor
+
 router = APIRouter()
 
 @router.post("/query")
@@ -9,3 +11,7 @@ async def query_endpoint(payload: dict, worker_task:BackgroundTasks):
     response=await process_query(user_message)
     worker_task.add_task(main_conductor,response)
     return response
+
+@router.websocket("/terminal_status")
+async def websocket_route(websocket: WebSocket):
+    await ws_controller.handle_websocket(websocket)
